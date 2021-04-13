878ab5fbc4147342e5847edda6a8a0403da5d829
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 01:34:45 +0200

    maybe old nodejs on netlify :/

--- netlify/functions/mayors.js
+++ netlify/functions/mayors.js
@@ -4,8 +4,9 @@ console.log("ZZ node version: ", process.version);
 console.log("ZX node versions: ", process.versions);
 
 exports.handler = async function (event, context) {
-  const channel =
-    event.headers["nightbot-channel"]?.split("&")[0].split("=")[1] || "default";
+  const channel = event.headers["nightbot-channel"]
+    ? event.headers["nightbot-channel"].split("&")[0].split("=")[1]
+    : "default";
 
   console.log(mayors[channel]);
 
