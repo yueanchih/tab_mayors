3a5dc4045e8db8f36f8143f7343ab201a92e3a5c
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 01:29:05 +0200

    test done, arr not needed

--- netlify/functions/mayors_array.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event, context) {
-  console.log("headers", event.headers);
-  console.log("multiValueHeaders", event.multiValueHeaders);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(["Mayor 1"]),
-  };
-};
