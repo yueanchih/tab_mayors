2825a8a4ce345fd72f6bb37beef172d0b2489304
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 01:06:02 +0200

    test with array render

---
+++ netlify/functions/mayors_array.js
@@ -0,0 +1,9 @@
+exports.handler = async function (event, context) {
+  console.log("headers", event.headers);
+  console.log("multiValueHeaders", event.multiValueHeaders);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(["Mayor 1"]),
+  };
+};
