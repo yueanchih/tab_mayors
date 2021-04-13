854008f9e6ac6dccd5d1eaeb13211cbe06006dea
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 01:41:21 +0200

    return string all the time

--- netlify/functions/mayors.js
+++ netlify/functions/mayors.js
@@ -1,17 +1,12 @@
 const mayors = require("./../models/mayors.json");
 
-console.log("ZZ node version: ", process.version);
-console.log("ZX node versions: ", process.versions);
-
 exports.handler = async function (event, context) {
   const channel = event.headers["nightbot-channel"]
     ? event.headers["nightbot-channel"].split("&")[0].split("=")[1]
     : "default";
 
-  console.log(mayors[channel]);
-
   return {
     statusCode: 200,
-    body: JSON.stringify(mayors[channel]),
+    body: JSON.stringify(mayors[channel].join(", ")),
   };
 };
