9f18ea55a7210d206c6e8ec5e16e1f73ae15048f
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 17:26:40 +0200

    first rought try deletemayors

--- netlify/functions/deletemayors.js
+++ netlify/functions/deletemayors.js
@@ -1,9 +1,56 @@
+const faunadb = require("faunadb");
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET,
+});
+
 exports.handler = async function (event, context) {
-  console.log("headers", event.headers);
-  console.log("multiValueHeaders", event.multiValueHeaders);
+  if (event.headers["nightbot-channel"]) {
+    const channel = event.headers["nightbot-channel"]
+      .split("&")[0]
+      .split("=")[1];
+
+    const faunaDBQuery = q.Map(
+      q.Paginate(q.Match(q.Index("mayors_by_channel"), channel)),
+      q.Lambda(
+        "mayors_by_channel",
+        q.Update(q.Var("mayors_by_channel"), {
+          data: {
+            channel: channel,
+            mayors: [],
+          },
+        })
+      )
+    );
+
+    return client
+      .query(faunaDBQuery)
+      .then((response) => {
+        console.log("1 response", JSON.stringify(response));
+
+        // const jj = response.data.length
+        //   ? [(response.data[0].data.mayors || []).join(", ")]
+        //   : [];
+        // console.log("mayors: ", jj);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify("Mayors fired."),
+        };
+      })
+      .catch((error) => {
+        console.log("error", error);
+
+        return callback(null, {
+          statusCode: 400,
+          body: JSON.stringify(error),
+        });
+      });
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Deleted all mayors" }),
+    body: JSON.stringify({ message: "None-nightbot request" }),
   };
 };
