96a3bc09d141500c425d57216702e8cb39d5738d
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 03:51:35 +0200

    faunadb over local json file

--- netlify/functions/mayors.js
+++ netlify/functions/mayors.js
@@ -1,12 +1,28 @@
-const mayors = require("./../models/mayors.json");
+const faunadb = require("faunadb");
 
-exports.handler = async function (event, context) {
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET,
+});
+
+exports.handler = async function (event, context, callback) {
   const channel = event.headers["nightbot-channel"]
     ? event.headers["nightbot-channel"].split("&")[0].split("=")[1]
     : "default";
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(mayors[channel].join(", ")),
-  };
+  return client
+    .query(q.Get(q.Ref(q.Collection("mayors"), "101")))
+    .then((response) => {
+      return {
+        statusCode: 200,
+        body: response.data[channel].join(", "),
+      };
+    })
+    .catch((error) => {
+      console.log("error", error);
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      });
+    });
 };
