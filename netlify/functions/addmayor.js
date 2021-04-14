61cdf0e1f998311bc404dffcd10009125a5e465c
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2021-04-14 02:03:52 +0200

    dirty, yet should work

--- netlify/functions/addmayor.js
+++ netlify/functions/addmayor.js
@@ -4,33 +4,40 @@ const mayors = require("./../models/mayors.json");
 exports.handler = async function (event, context) {
   console.log("headers", event.headers);
 
-  const channel = event.headers["nightbot-channel"]
-    ? event.headers["nightbot-channel"].split("&")[0].split("=")[1]
-    : "default";
+  if (event.headers["nightbot-channel"]) {
+    const channel = event.headers["nightbot-channel"]
+      ? event.headers["nightbot-channel"].split("&")[0].split("=")[1]
+      : "default";
 
-  const mayor = event.queryStringParameters["mayor"];
+    const mayor = event.queryStringParameters["mayor"];
 
-  if (channel === "default") {
-    mayors[event.headers["nightbot-channel"].split("&")[0].split("=")[1]] = [
-      mayor,
-    ];
-  } else {
-    mayors[event.headers["nightbot-channel"].split("&")[0].split("=")[1]].push(
-      mayor
-    );
-  }
-
-  fs.writeFile(
-    "./../models/mayors.json",
-    JSON.stringify(mayors, null, 2),
-    (err) => {
-      if (err) throw err;
-      console.log("Data written to file");
+    if (channel === "default") {
+      mayors[event.headers["nightbot-channel"].split("&")[0].split("=")[1]] = [
+        mayor,
+      ];
+    } else {
+      mayors[
+        event.headers["nightbot-channel"].split("&")[0].split("=")[1]
+      ].push(mayor);
     }
-  );
+
+    fs.writeFile(
+      "./../models/mayors.json",
+      JSON.stringify(mayors, null, 2),
+      (err) => {
+        if (err) throw err;
+        console.log("Data written to file");
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Added mayor: X" }),
+    };
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Added mayor: X" }),
+    body: JSON.stringify({ message: "None-nightbot request" }),
   };
 };
