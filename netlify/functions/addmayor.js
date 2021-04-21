b8108a436e2162835fae76f5bb7ebb59396e5684
yueanchih <82918629+yueanchih@users.noreply.github.com>
2021-04-21 10:36:43 +0800

    Update addmayor.js

--- netlify/functions/addmayor.js
+++ netlify/functions/addmayor.js
@@ -2,7 +2,7 @@ const faunadb = require("faunadb");
 
 const q = faunadb.query;
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET;
+  secret: process.env.FAUNADB_SERVER_SECRET,
 });
 
 exports.handler = async function (event, context) {
